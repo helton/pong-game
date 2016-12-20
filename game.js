b5f68db74ad4badaac67e6e11ed2996709d9c41a
Helton Carlos de Souza <heltoncarlossouza@gmail.com>
2016-12-20 21:48:43 -0200

    Refactoring Code - Drawed middle line

--- game.js
+++ game.js
@@ -1,9 +1,32 @@
-function run() {
-  const canvas = document.querySelector("#gameCanvas");
-  const context = gameCanvas.getContext("2d");
+let canvas;
+let context;
 
-  context.fillStyle = "black";
-  context.fillRect(0, 0, canvas.width, canvas.height);
+function setup() {
+  canvas = document.getElementById("gameCanvas");
+  context = gameCanvas.getContext("2d");
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+}
+
+function drawRect(color, coord) {
+  context.fillStyle = color;
+  context.fillRect(coord.x, coord.y, coord.width, coord.height);
+}
+
+function run() {
+  setup();
+  drawRect("black", {
+    x: 0,
+    y: 0,
+    width: canvas.width,
+    height: canvas.height
+  });
+  drawRect("white", {
+    x: canvas.width/2,
+    y: 0,
+    width: 1,
+    height: canvas.height
+  });
 }
 
 window.onload = run;
